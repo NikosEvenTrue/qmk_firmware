56cf13f09a2c56227739e18105053e7d649c2a98
Niko Seven <nikita.xakimov.77@gmail.com>
2026-01-10 04:43:06 +0300

    add lalt, rlat and fix LGUI

--- keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
@@ -228,7 +228,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|      |    |      |------+------+------+------+------+------|
      * | Tab  |   A  |   S  |   D  |   F  |   G  |------|    |------|   H  |   J  |   K  |   L  |      | Entr |
      * |      |   ф  |   ы  |   в  |   а  |   п  |      |    |      |   р  |   о  |   л  |   д  |   ж  |      |
-     * |------+------+------+------+------+------|XXXXXX|    |XXXXXX|------+------+------+------+------+------|
+     * |------+------+------+------+------+------| LAlt |    |XXXXXX|------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |------|    |------|   N  |  M   |      |      |      |RShift|
      * |      |   я  |   ч  |   с  |   м  |   и  |      |    |      |   т  |  ь  [ъ]  б  |   ю  |   э  |      |
      * `----------------------------------------/       /     \      \-----------------------------------------'
@@ -239,8 +239,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   XXXXXXX,        XXXXXXX, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,        XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RSFT,
-                                            KC_LGUI, KC_SPC,  MO(1),   KC_LCTL,        KC_LALT,  MO(2),   NIKO_CHNG_LNG, XXXXXXX
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,        XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RSFT,
+                                            XXXXXXX, KC_SPC,  MO(1),   KC_LCTL,        KC_RALT,  MO(2),   NIKO_CHNG_LNG, XXXXXXX
     ),
 
     /*
@@ -251,7 +251,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |XXXXXX|   !  |   @  |   #  |   $  |   %  |------|    |------|   ~  |   (  |   )  |   [  |   ]  |   >  |
      * |------+------+------+------+------+------|      |    |      |------+------+------+------+------+------|
      * |XXXXXX|   +  |   -  |   *  |   /  |   \  |------|    |------|   `  |   '  |   "  |   :  |   ;  |   <  |
-     * |------+------+------+------+------+------|XXXXXX|    |XXXXXX|------+------+------+------+------+------|
+     * |------+------+------+------+------+------| LAlt |    |XXXXXX|------+------+------+------+------+------|
      * |XXXXXX|XXXXXX|   |  |   &  |   _  |   =  |------|    |------|   {  |   }  |   ,  |   .  |   ?  |XXXXXX|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
      *                  |XXXXXX|XXXXXX|XXXXXX|XXXXXX| /       /       \      \ | CAPS | MO(3)| XXXXXX |XXXXXX|
@@ -262,7 +262,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX,      XXXXXXX,      NIKO_UC_NUMO, XXXXXXX,      XXXXXXX,      XXXXXXX,        XXXXXXX, XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,       XXXXXXX,       XXXXXXX,
         XXXXXXX, NIKO_UC_EXLM, NIKO_UC_AT,   NIKO_UC_HASH, NIKO_UC_DLR,  NIKO_UC_PERC,                          NIKO_UC_TILD,      NIKO_UC_LPRN, NIKO_UC_RPRN, NIKO_UC_LBRC,  NIKO_UC_RBRC,  NIKO_UC_GT,
         XXXXXXX, NIKO_UC_PLUS, NIKO_UC_MINS, NIKO_UC_ASTR, NIKO_UC_SLSH, NIKO_UC_BSLS,                          NIKO_UC_GRV,  NIKO_UC_QUOT, NIKO_UC_DQUO, NIKO_UC_COLN,  NIKO_UC_SCLN,  NIKO_UC_LT,
-        XXXXXXX, XXXXXXX,      NIKO_UC_PIPE, NIKO_UC_AMPR, NIKO_UC_UNDS, NIKO_UC_EQL,      XXXXXXX,        XXXXXXX, NIKO_UC_LCBR, NIKO_UC_RCBR, NIKO_UC_COMM, NIKO_UC_DOT,   NIKO_UC_QUES,  XXXXXXX,
+        XXXXXXX, XXXXXXX,      NIKO_UC_PIPE, NIKO_UC_AMPR, NIKO_UC_UNDS, NIKO_UC_EQL,      KC_LALT,        XXXXXXX, NIKO_UC_LCBR, NIKO_UC_RCBR, NIKO_UC_COMM, NIKO_UC_DOT,   NIKO_UC_QUES,  XXXXXXX,
                                             XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                   KC_CAPS,      MO(3),   XXXXXXX,      XXXXXXX
     ),
 
@@ -274,10 +274,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |XXXXXX|  1   |  2   |  3   |  4   |  5   |------|    |------|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX| Bspc |
      * |------+------+------+------+------+------|      |    |      |------+------+------+------+------+------|
      * |XXXXXX|  6   |  7   |  8   |  9   |  0   |------|    |------|XXXXXX| Left | Right| Down |  Up  |XXXXXX|
-     * |------+------+------+------+------+------|XXXXXX|    |XXXXXX|------+------+------+------+------+------|
+     * |------+------+------+------+------+------| LAlt |    |XXXXXX|------+------+------+------+------+------|
      * |LShift|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|------|    |------|XXXXXX| Home | End  | PgDn | PgUp |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                  |XXXXXX| Space| MO(3)| LGUI | /       /       \      \ | LAlt |XXXXXX| Enter|XXXXXX|
+     *                  |XXXXXX| Space| MO(3)| LGUI | /       /       \      \ | RAlt |XXXXXX| Enter|XXXXXX|
      *                  |      |      |      |      |/       /         \      \|      |      |      |      |
      *                  `------------------------------------'           '------''---------------------------'
      */
@@ -285,8 +285,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX,        XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         XXXXXXX, KC_1, KC_2, KC_3, KC_4, KC_5,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
         XXXXXXX, KC_6, KC_7, KC_8,  KC_9, KC_0,                          KC_APP, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP,   XXXXXXX,
-        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, NIKO_WIN_T, NIKO_WIN_B, XXXXXXX,        XXXXXXX, XXXXXXX, KC_HOME, KC_END,  KC_PGDN, KC_PGUP, KC_RSFT,
-                                            KC_LGUI, KC_SPC,  MO(3),   KC_LCTL,        KC_LALT, XXXXXXX, KC_ENT,  XXXXXXX
+        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, NIKO_WIN_T, NIKO_WIN_B, KC_LALT,        XXXXXXX, XXXXXXX, KC_HOME, KC_END,  KC_PGDN, KC_PGUP, KC_RSFT,
+                                            XXXXXXX, KC_SPC,  KC_LGUI,   KC_LCTL,        KC_RALT, XXXXXXX, KC_ENT,  XXXXXXX
     ),
 
     /*
