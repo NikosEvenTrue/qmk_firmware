4d324606e4156da893c0899a7960f5f7ec273223
Niko Seven <nikita.xakimov.77@gmail.com>
2026-02-01 23:51:22 +0300

    move layer1 key

--- keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/unicode/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         XXXXXXX, KC_1, KC_2, KC_3, KC_4, KC_5,      XXXXXXX,        XXXXXXX, KC_6, KC_7, KC_8,  KC_9, KC_0,  XXXXXXX,
         XXXXXXX,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
-        XXXXXXX,  MT(MOD_RGUI, KC_A),    MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),    MT(MOD_LSFT, KC_F),    LT(1, KC_G),                             LT(1, KC_H),    MT(MOD_RSFT, KC_J),    MT(MOD_LCTL, KC_K),    MT(MOD_LALT, KC_L),    MT(MOD_RGUI, KC_SCLN), XXXXXXX,
+        MO(1),  MT(MOD_RGUI, KC_A),    MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),    MT(MOD_LSFT, KC_F), KC_G,                         KC_H,    MT(MOD_RSFT, KC_J),    MT(MOD_LCTL, KC_K),    MT(MOD_LALT, KC_L),    MT(MOD_RGUI, KC_SCLN), MO(1),
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,        KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
                                             XXXXXXX, KC_TAB,  MO(2),   KC_SPC,        KC_BSPC,  MO(2),   KC_ENT, XXXXXXX
     ),
